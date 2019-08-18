f781c640a3469a05e6b9f83efa8fd44fe79e85ba
michail <refs1@yandex.ru>
2019-08-18 03:05:14 +0300

    parallax

---
+++ src/js/parallax.js
@@ -0,0 +1,71 @@
+const block = document.querySelectorAll('.block-img');
+// var blockImg = document.querySelector('.block-img');
+// var blockImgBottom = blockImg.getBoundingClientRect().bottom;
+// var blockImgHeight = blockImg.clientHeight;
+var blockImgBottom;
+var titleTop;
+var blockImgTop;
+var titlePositionTop;
+
+var arrBlockImgBottom = []; //массив для значение от начала страницы до низа блока
+var arrTitleTop = [];//массив для значение от начала страницы до верха заголовка
+var arrTitlePositionTop = [];//массив для значение позиций title style.top
+var arrBlockImgTop = [];//массив для значение от начала страницы до вверха блока
+block.forEach((title, index) => {
+  blockImgBottom = title.getBoundingClientRect().bottom;
+  blockImgTop = title.getBoundingClientRect().top;
+  titleTop = title.querySelector('.block-img__title').getBoundingClientRect().top;
+  titlePositionTop = getComputedStyle(title.querySelector('.block-img__title')).top;
+  // blockImgHeight = title.clientHeight;
+
+  arrTitleTop.push(titleTop);
+  arrBlockImgBottom.push(blockImgBottom);
+  arrTitlePositionTop.push(titlePositionTop);
+  arrBlockImgTop.push(blockImgTop)
+});
+
+window.addEventListener('scroll', function (e) {
+  // var blockImg = -(blockImgBottom + blockImgHeight) + window.pageYOffset;
+
+  const scroll = window.pageYOffset;
+  block.forEach((title, index) => {
+    if (arrBlockImgTop[index] < scroll && arrBlockImgBottom[index] > scroll) {
+      console.log(index)
+      var blockImgHeight = title.clientHeight;
+      var titleC = (scroll - arrBlockImgTop[index]);
+      console.log(index+ "::" +titleC)
+      title.querySelector('.block-img__title').style.top = `${parseInt(arrTitlePositionTop[index])+titleC/2}px`;
+    }
+  });
+});
+
+// внизу код незадействован
+// function scrolling(e) {
+//
+//   containers.forEach(listItem => {
+//     if (isPartiallyVisible(listItem)) {
+//
+//     } else {
+//       listItem.classList.remove("active");
+//     }
+//   })
+// }
+
+
+function isPartiallyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+  var height = elementBoundary.height;
+  return ((top + height >= 0) && (height + document.documentElement.scrollHeight >= bottom));
+}
+
+function isFullyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+
+  return ((top >= 0) && (bottom <= document.documentElement.scrollHeight));
+}
